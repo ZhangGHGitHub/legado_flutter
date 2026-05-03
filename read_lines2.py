28a93e64521d63933a6f78bd515cdc3cba6616d9
admin <admin@local.dev>
2026-05-03 22:24:12 +0800

    chore: 添加 .gitignore，排除临时文件

--- read_lines2.py
+++
@@ -1,4 +0,0 @@
-import io, sys; sys.stdout=io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-lines = open('C:/Users/admin/Projects/legado_flutter/lib/pages/reader/reader_page.dart', encoding='utf-8').readlines()
-for i in range(1060, 1106):
-    print(f'{i+1}: {lines[i].rstrip()}')
